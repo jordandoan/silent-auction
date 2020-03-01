dd1c00fbe2d927bcc15ebb197a9b30a66c7cec78
Jordan <jordandoan@hotmail.com>
2020-03-01 09:46:32 -0800

    removed more components

--- src/onepirate/Home.js
+++ src/onepirate/Home.js
@@ -1,23 +1,17 @@
 import withRoot from './modules/withRoot';
 // --- Post bootstrap -----
 import React from 'react';
-import ProductCategories from './modules/views/ProductCategories';
-import ProductSmokingHero from './modules/views/ProductSmokingHero';
+
 import AppFooter from './modules/views/AppFooter';
 import ProductHero from './modules/views/ProductHero';
 import ProductValues from './modules/views/ProductValues';
-import ProductHowItWorks from './modules/views/ProductHowItWorks';
-import ProductCTA from './modules/views/ProductCTA';
+
 
 function Index() {
   return (
     <React.Fragment>
       <ProductHero />
       <ProductValues />
-      <ProductCategories />
-      <ProductHowItWorks />
-      <ProductCTA />
-      <ProductSmokingHero />
       <AppFooter />
     </React.Fragment>
   );
