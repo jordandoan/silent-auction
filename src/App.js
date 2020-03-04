75dd3555f595e5cf27f0667bb0f306d1de4d0431
Jordan <jordandoan@hotmail.com>
2020-03-04 10:58:20 -0800

    Removed landing page, added navbar

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React, { useState, useContext } from 'react';
 import 'typeface-roboto';
 import './App.css';
 import { Route } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+
 import UserContext from './contexts/UserContext';
 import RedirectAuthRoute from './utils/RedirectAuthRoute';
-import Home from './onepirate/Home';
+
+
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token"));
   return (
     <UserContext.Provider value={{token, setToken}}>
       <div className="App">
+        <NavBar />
         <RedirectAuthRoute path="/signin" component={SignIn} />
         <RedirectAuthRoute path="/signup" component={SignUp} />
-        <Route exact path="/" component={Home}/>
       </div>
     </UserContext.Provider>
   );
