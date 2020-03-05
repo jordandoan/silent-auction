f38306689cf4d90cbea594fa4127196927d98f76
Jordan <jordandoan@hotmail.com>
2020-03-05 14:48:03 -0800

    More work with detailed auction

--- src/components/DetailedAuction.js
+++ src/components/DetailedAuction.js
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grid, FormRow } from '@material-ui/core/';
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+import isFuture from 'date-fns/isFuture';
+import { Container, Grid, Paper, Button, FormControl, OutlinedInput, InputLabel } from '@material-ui/core/';
 
+import BidInfo from './BidInfo';
+import Byline from './Byline';
 import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const DetailedAuction = ({ history, match, location}) => {
-  const [auction, setAuction] = useState({})
+  const [auction, setAuction] = useState({});
+  const [timeLeft, setTime] = useState([false, 0]);
+
   useEffect(() => {
     axiosWithAuth().get(`api/auctions/${match.params.id}`)
       .then(res => {
         setAuction(res.data)
+        let arr = [true, 0];
+        let end = new Date(res.data.date_ending);
+        if (!isFuture(end)) {
+          arr[0] = false;
+        }
+        arr[1] = formatDistanceToNow(end);
+        setTime(arr);
       })
   }, [])
-  console.log(auction);
+
   return (
     <Container>
       <Grid container spacing={2}>
@@ -19,19 +32,36 @@ const DetailedAuction = ({ history, match, location}) => {
           <h2>{auction.name}</h2>
           <img style={{width: '30%'}} src={auction.image} />
           <p>{auction.description}</p>
-          <p>Sold by {auction.first_name} @<span>{auction.seller}</span></p>
+          <p>Sold by <Byline first_name={auction.first_name} username={auction.seller} /></p>
         </Grid>
         <Grid item xs={3}>
-          <p>
-            <span>
-              {auction.starting_price}
-            </span>
-            <span>
-              {auction.bids && auction.bids.length ? auction.bids[auction.bids.length - 1]['price'] : ""}
-            </span>
-          </p>
-          <p>{auction.starting_date}</p>
-          <p>{auction.ending_date}</p>
+          <Paper elevation={3}>
+            <h3> Info </h3>
+            <p>
+              <span>Price:</span>
+              <span>
+                ${`${auction.starting_price} `}
+              </span>
+              
+              <span>
+                {auction.bids && auction.bids.length ? `$${auction.bids[auction.bids.length - 1]['price']} ` : ""}
+                {timeLeft[0] ? timeLeft[1] : "Auction has ended!"}
+              </span>
+            </p>
+            <p>Start date: {new Date(auction.date_starting).toString().substring(0,21)}</p>
+            <p>End date: {new Date(auction.date_ending).toString().substring(0,21)}</p>
+            <div>
+              <h4>Bid History</h4>
+              {auction.bids && auction.bids.map((bid, i) => <BidInfo key={i} last={i === auction.bids.length - 1} bid={bid}/>)}
+            </div>
+            <FormControl variant="outlined">
+              <InputLabel htmlFor="component-outlined">Price</InputLabel>
+              <OutlinedInput id="component-outlined" label="Name" placeholder="200"/>
+            </FormControl>
+            <Button variant="contained" color="primary" disabled={!timeLeft[0]}>
+              Place bid
+            </Button>
+          </Paper>
         </Grid>
       </Grid>
     </Container>
