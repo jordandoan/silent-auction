eb2cb31a22c9bd9afb3d0f48fd2d71f986b0067d
Jordan <jordandoan@hotmail.com>
2020-03-05 16:33:12 -0800

    Minimal styling for auction form

--- src/components/AuctionForm.js
+++ src/components/AuctionForm.js
@@ -1,56 +1,103 @@
+import 'date-fns';
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import { KeyboardDatePicker, KeyboardTimePicker } from '@material-ui/pickers';
+import { Grid, FormControl, InputLabel, Input, TextField } from '@material-ui/core/';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
 const AuctionForm = () => {
-  const [selectedDate, setSelectedDate] = React.useState(new Date('2014-08-18T21:11:54'));
+  const [startDate, setStart] = React.useState(new Date());
+  const [endDate, setEnd] = React.useState(new Date().setDate(new Date().getDay() + 8))
   const handleDateChange = date => {
-    setSelectedDate(date);
+    setStart(date);
   };
+
   return (
     <div>
-      <Grid container justify="space-around">
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="Date picker inline"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-        <KeyboardDatePicker
-          margin="normal"
-          id="date-picker-dialog"
-          label="Date picker dialog"
-          format="MM/dd/yyyy"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-        <KeyboardTimePicker
-          margin="normal"
-          id="time-picker"
-          label="Time picker"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change time',
-          }}
-        />
+      <Grid container justify="center" direction="column" spacing={3}>
+        <Grid item> 
+          <TextField variant="outlined" label="Title"placeholder="iPhone SE"/>
+
+        </Grid>
+        <Grid item>
+          <TextField type="number" variant="outlined" label="Price" placeholder={150} />
+
+        </Grid>
+        <Grid item>
+          <TextField multiline={true} variant="outlined" label="Description" placeholder="A beautiful iPhone!"/>
+
+        </Grid>
+        <Grid item>
+          <TextField variant="outlined" label="Image URL" placeholder="https://...."/>
+
+        </Grid>
       </Grid>
-      Auction Form
-      Title
-      Price
-      Date Start
-      Date End
-      Description
-      Image
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={2}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Start date"
+              value={startDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+          </Grid>
+          <Grid item spacing={2} xs={2}>
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="Start time"
+              value={startDate}
+              onChange={handleDateChange}
+              size="small"
+              KeyboardButtonProps={{
+                'aria-label': 'change time',
+              }}
+            />
+          </Grid>
+        </Grid>
+      </MuiPickersUtilsProvider>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="center" spacing={2}>
+          <Grid item xs={2}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="End date"
+              value={endDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="End time"
+              value={endDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change time',
+              }}
+            />
+          </Grid>
+        </Grid>
+      </MuiPickersUtilsProvider>
     </div>
   )
 }
