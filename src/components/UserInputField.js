ea3c018a17840b85d404fa064d2b670c81bea349
Jordan <jordandoan@hotmail.com>
2020-03-25 12:05:28 -0700

    Consistent spacing for each field

--- src/components/UserInputField.js
+++ src/components/UserInputField.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+
 import Typography from '@material-ui/core/Typography';
 import styles from './UserInput.module.scss';
 
@@ -12,24 +15,24 @@ const UserInputField = ({data, fields, edit, type, handleChange, handleFieldView
   }
 
   return (
-    <Grid container justify="space-between" md={8} className={styles.main}>
-      <Grid item xs={3}>
-        {header[type]}:
+    <Grid container justify="space-around" md={8} className={styles.main} alignItems="center">
+      <Grid item xs={3} className={styles.title}>
+        <Typography align="left" variant="overline">{header[type]}:</Typography>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={6} className={styles.test}>
         {edit[type]
-          ? <input name={type} value={fields[type]} onChange={handleChange}/>
-          : <span>{data[type]}</span>
+          ? <TextField fullWidth={true} className={styles.test} name={type} value={fields[type]} onChange={handleChange}/>
+          : <Typography align="left">{data[type]}</Typography>
         }
       </Grid>
-      <Grid item xs={3}>
+      <Grid item xs={3} align="right">
         {edit[type]
           ? 
             <>
-              <Typography variant="subtitle2" display="initial" className={styles.button} onClick={(e) => {handleSubmit(e, type)}}>Save</Typography>
-              <Typography variant="subtitle2" display="initial" className={styles.button} onClick={() => {handleFieldView(type)}}>Cancel</Typography>
+              <Typography  color="textSecondary" variant="subtitle2" display="initial" className={styles.button} onClick={(e) => {handleSubmit(e, type)}}>Save</Typography>
+              <Typography  color="textSecondary" variant="subtitle2" display="initial" className={styles.button} onClick={() => {handleFieldView(type)}}>Cancel</Typography>
             </>
-          : <Typography variant="subtitle2" display="initial" className={styles.button} onClick={()=>{handleFieldView(type)}}>Edit</Typography>
+          : <Typography  color="textSecondary" variant="subtitle2" display="initial" className={styles.button} onClick={()=>{handleFieldView(type)}}>Edit</Typography>
         }
       </Grid>
     </Grid>
